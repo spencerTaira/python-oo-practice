255e61dee8c18f2850a9125c6f8b3386c3183550
Spencer Taira <spencermtaira@gmail.com>
2022-10-11 15:25:54 -0700

    completed serial.py

--- serial.py
+++ serial.py
@@ -1,6 +1,9 @@
+from re import I
+
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +20,16 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start=0):
+        """Initialize serial code with start parameter"""
+        self.initial_val = start - 1
+        self.start_val = start - 1
+
+    def generate(self):
+        """Generate a new serial code then increment start value"""
+        self.start_val += 1
+        return self.start_val
+
+    def reset(self):
+        """Resets self.start to intiial start parameter"""
+        self.start_val = self.initial_val
